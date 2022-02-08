b846940c316a8bf7a70ee47177d5a5ff1cebfac4
VeekshitaPudi <pudiveekshita580@gmail.com>
2022-02-08 16:36:13 +0530

    new_commit

---
+++ User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        
+        public string UserId { get; set; }  
+
+        public string EmailId { get; set; }
+
+        public string MobileNo { get; set; }    
+
+    }
+}
